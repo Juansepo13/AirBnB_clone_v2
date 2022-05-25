e6b4b85480d8b55e2b4a1af251369d6d75b119ee
Juansepo13 <juancho_s1314@hotmail.com>
2022-05-24 19:58:07 -0500

    act file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
+"""Write a script that start a FlaskWeb application """
 
-"""Write a script that start a FlaskWeb application
-   Listening on 0.0.0.0 in Port: 5000 """
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_HBNB():
-    """return a message"""
-    return "Hello HBNB!!"
+@app.route("/", strict_slashes=False)
+def task_0():
+    """ Task 0 Function """
+    return("Hello HBNB!")
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=True)
