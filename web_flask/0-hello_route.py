f80e6504a3ee5f6fe0ed574d2a385ec214e0793f
Juansepo13 <juancho_s1314@hotmail.com>
2022-05-24 19:55:43 -0500

    act fiile with pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-""" 
-Write a script that start a FlaskWeb application
-Listening on 0.0.0.0 in Port: 5000
-"""
+"""Write a script that start a FlaskWeb application
+   Listening on 0.0.0.0 in Port: 5000 """
 from flask import Flask
 app = Flask(__name__)
 
@@ -12,6 +10,6 @@ def hello_HBNB():
     """return a message"""
     return "Hello HBNB!!"
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
-  
